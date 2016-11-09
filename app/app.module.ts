b5e8df12c62f34d2f65ae7a6ce85526236e4e38a
xouxou <xouxou_coursera@hotmail.com>
2016-11-09 17:24:35 +0100

    - http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,32 +1,26 @@
 import { NgModule}      from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 import { FormsModule }  from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService }         from './hero.service';
-import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
     imports:        [
                         BrowserModule,
                         FormsModule,
-                        RouterModule.forRoot([
-                            {
-                                path: '',
-                                redirectTo: '/dashboard',
-                                pathMatch: 'full'
-                            },
-                            {
-                                path: 'heroes',
-                                component: HeroesComponent
-                            },
-                            {
-                                path: 'dashboard',
-                                component: DashboardComponent
-                            }
-                        ])
+                        HttpModule,
+                        InMemoryWebApiModule.forRoot(InMemoryDataService),
+                        AppRoutingModule
                     ],
     declarations:   [
                         AppComponent,
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -2,70 +2,14 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router }   from '@angular/router';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-heroes',
-    template: `
-        <ul class="heroes">
-            <li 
-                *ngFor="let hero of heroes"
-                [class.selected]="hero === selectedHero"
-                (click)="onSelect(hero)">
-                <span class="badge">{{hero.id}}</span> {{hero.name}}
-            </li>
-        </ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-        `,
+    templateUrl: 'heroes.component.html',
     providers: [HeroService],
-    styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`]
+    styleUrls: ['heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
@@ -73,7 +17,11 @@ export class HeroesComponent implements OnInit {
     heroes : Hero[];
     selectedHero : Hero;
 
-    constructor(private heroService: HeroService) { }
+    constructor( private router: Router,private heroService: HeroService) { }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
@@ -85,5 +33,15 @@ export class HeroesComponent implements OnInit {
 
     ngOnInit(): void {
         this.getHeroes();
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+                this.heroes.push(hero);
+                this.selectedHero = null;
+            });
     }
 }
